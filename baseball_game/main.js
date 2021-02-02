54f33a6badb128f9eb3f483eaae4ac978a4c6098
uuuuujin <yearn8546@gmail.com>
2021-02-02 21:02:43 +0900

    기능&디자인 수정

--- baseball_game/main.js
+++ baseball_game/main.js
@@ -28,6 +28,8 @@ function getRandom() {
     document.getElementById("ball_cnt").innerText = 0;
     document.getElementById("gameStart").innerText = "게임시작!"
 
+
+
 }
 
 // 지우기 function
@@ -38,6 +40,8 @@ function remove() {
     document.getElementById("first").innerText = null;
     document.getElementById("second").innerText = null;
     document.getElementById("third").innerText = null;
+    document.getElementById("strike_cnt").innerText = 0;
+    document.getElementById("ball_cnt").innerText = 0;
 }
 
 // 게임 플레이 function
@@ -72,10 +76,10 @@ function one() {
     if (var1 === null) {
         document.getElementById("first").innerText = 1;
         var1 = 1;
-    } else if (var2===null) {
+    } else if ((var2===null) && (var1!=1)) {
         document.getElementById("second").innerText = 1;
         var2 = 1;
-    } else {
+    } else if ((var3===null)&&(var1!=1)&&(var2!=1)) {
         document.getElementById("third").innerText = 1;
         var3 = 1;
     }
@@ -85,10 +89,10 @@ function two() {
     if (var1 === null) {
         document.getElementById("first").innerText = 2;
         var1 = 2;
-    } else if (var2===null) {
+    } else if ((var2===null) && (var1!=2)) {
         document.getElementById("second").innerText = 2;
         var2 = 2;
-    } else {
+    } else if ((var3===null)&&(var1!=2)&&(var2!=2)) {
         document.getElementById("third").innerText = 2;
         var3 = 2;
     }
@@ -98,10 +102,10 @@ function three() {
     if (var1 === null) {
         document.getElementById("first").innerText = 3;
         var1 = 3;
-    } else if (var2===null) {
+    } else if ((var2===null) && (var1!=3)) {
         document.getElementById("second").innerText = 3;
         var2 = 3;
-    } else {
+    } else if ((var3===null)&&(var1!=3)&&(var2!=3)) {
         document.getElementById("third").innerText = 3;
         var3 = 3;
     }
@@ -111,10 +115,10 @@ function four() {
     if (var1 === null) {
         document.getElementById("first").innerText = 4;
         var1 = 4;
-    } else if (var2===null) {
+    } else if ((var2===null) && (var1!=4)) {
         document.getElementById("second").innerText = 4;
         var2 = 4;
-    } else {
+    } else if ((var3===null)&&(var1!=4)&&(var2!=4)) {
         document.getElementById("third").innerText = 4;
         var3 = 4;
     }
@@ -124,10 +128,10 @@ function five() {
     if (var1 === null) {
         document.getElementById("first").innerText = 5;
         var1 = 5;
-    } else if (var2===null) {
+    } else if ((var2===null) && (var1!=5)) {
         document.getElementById("second").innerText = 5;
         var2 = 5;
-    } else {
+    } else if ((var3===null)&&(var1!=5)&&(var2!=5)) {
         document.getElementById("third").innerText = 5;
         var3 = 5;
     }
@@ -137,10 +141,10 @@ function six() {
     if (var1 === null) {
         document.getElementById("first").innerText = 6;
         var1 = 6;
-    } else if (var2===null) {
+    } else if ((var2===null) && (var1!=6)) {
         document.getElementById("second").innerText = 6;
         var2 = 6;
-    } else {
+    } else if ((var3===null)&&(var1!=6)&&(var2!=6)) {
         document.getElementById("third").innerText = 6;
         var3 = 6;
     }
@@ -150,10 +154,10 @@ function seven() {
     if (var1 === null) {
         document.getElementById("first").innerText = 7;
         var1 = 7;
-    } else if (var2===null) {
+    } else if ((var2===null) && (var1!=7)) {
         document.getElementById("second").innerText = 7;
         var2 = 7;
-    } else {
+    } else if ((var3===null)&&(var1!=7)&&(var2!=7)){
         document.getElementById("third").innerText = 7;
         var3 = 7;
     }
@@ -163,10 +167,10 @@ function eight() {
     if (var1 === null) {
         document.getElementById("first").innerText = 8;
         var1 = 8;
-    } else if (var2===null) {
+    } else if ((var2===null) && (var1!=8)) {
         document.getElementById("second").innerText = 8;
         var2 = 8;
-    } else {
+    } else if ((var3===null)&&(var1!=8)&&(var2!=8)){
         document.getElementById("third").innerText = 8;
         var3 = 8;
     }
@@ -176,10 +180,10 @@ function nine() {
     if (var1 === null) {
         document.getElementById("first").innerText = 9;
         var1 = 9;
-    } else if (var2===null) {
+    } else if ((var2===null) && (var1!=9)) {
         document.getElementById("second").innerText = 9;
         var2 = 9;
-    } else {
+    } else if ((var3===null)&&(var1!=9)&&(var2!=9)){
         document.getElementById("third").innerText = 9;
         var3 = 9;
     }
